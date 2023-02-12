cbde2d262c75f42a4f4206d493980b65c1b49aee
0xEddie <eomeasoo@ualberta.ca>
2023-02-12 16:46:34 -0700

    Import and stage root router

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
 import './index.css';
 import '@fontsource/barlow-condensed';
 import '@fontsource/bellefair';
 import '@fontsource/barlow';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Root from './routes/Root';
+
+const router = createBrowserRouter([
+	{
+		path: "/",
+		element: <Root />
+	}
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
 	<React.StrictMode>
-		<App />
+		<RouterProvider router={router} />
 	</React.StrictMode>
 );
---
+++ src/routes/Root.jsx
@@ -0,0 +1,10 @@
+export default function Root() {
+	return (
+		<header>
+			<img src="/src/assets/shared/logo.svg" />
+			<span>
+				~~navbar~~
+			</span>
+		</header>
+	)
+}
