bff972c786f9e8b3ae1121b353ecbdee04c6b818
0xEddie <eomeasoo@ualberta.ca>
2023-03-17 12:10:53 -0600

    styling

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,18 +3,18 @@ import { Link } from 'react-router-dom';
 export default function Home() {
 	return (
 		<main id="main" className="grid-container grid-container--home">
-			<div>
-				<h1 className="ff-sans-cond fs-500 letter-spacing-3 uppercase">
+			<article>
+				<h1 className="ff-sans-cond fs-500 text-accent letter-spacing-3 uppercase">
 					So, you want to travel to <br />
 					<span className="ff-serif fs-900 text-white">space</span>
 				</h1>
-				<p>
+				<p className="description">
 					Let’s face it; if you want to go to space, you might as well genuinely
 					go to outer space and not hover kind of on the edge of it. Well sit
 					back, and relax because we’ll give you a truly out of this world
 					experience!
 				</p>
-			</div>
+			</article>
 			<div>
 				{/* TODO where does this link point to? */}
 				<Link
