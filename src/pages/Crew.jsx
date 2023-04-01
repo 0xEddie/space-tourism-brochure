d2183bec9a4b26f0c7798834136be04543b91219
0xEddie <eomeasoo@ualberta.ca>
2023-03-31 21:15:13 -0600

    Implement crew tab functionality !!!

--- src/pages/Crew.jsx
+++ src/pages/Crew.jsx
@@ -1,6 +1,10 @@
 import './Crew.css';
+import { useState } from 'react';
+import { crew } from '../data.json';
 
 export default function Crew() {
+	const [crewId, setCrewId] = useState(0);
+
 	return (
 		<main id="main" className="grid-container grid-container--crew flow">
 			<h1 className="numbered-title">
@@ -8,40 +12,50 @@ export default function Crew() {
 			</h1>
 
 			<div className="dot-indicators flex" role="tablist">
-				<button aria-selected="true" type="button" role="tab">
+				<button
+					aria-selected={crewId === 0}
+					type="button"
+					role="tab"
+					onClick={() => setCrewId(0)}
+				>
 					<span className="sr-only">The commander</span>
 				</button>
-				<button aria-selected="false" type="button" role="tab">
+				<button
+					aria-selected={crewId === 1}
+					type="button"
+					role="tab"
+					onClick={() => setCrewId(1)}
+				>
 					<span className="sr-only">The mission specialist</span>
 				</button>
-				<button aria-selected="false" type="button" role="tab">
+				<button
+					aria-selected={crewId === 2}
+					type="button"
+					role="tab"
+					onClick={() => setCrewId(2)}
+				>
 					<span className="sr-only">The pilot</span>
 				</button>
-				<button aria-selected="false" type="button" role="tab">
-					<span className="sr-only">The crew engineer</span>
+				<button
+					aria-selected={crewId === 3}
+					type="button"
+					role="tab"
+					onClick={() => setCrewId(3)}
+				>
+					<span className="sr-only">The flight engineer</span>
 				</button>
 			</div>
 
 			<article className="crew-details">
 				<header className="flow flow--space-small">
-					<h2 className="fs-600 ff-serif uppercase">Commander</h2>
-					<p className="fs-700 uppercase ff-serif">Douglas Hurley</p>
+					<h2 className="fs-600 ff-serif uppercase">{crew[crewId].role}</h2>
+					<p className="fs-700 uppercase ff-serif">{crew[crewId].name}</p>
 				</header>
-				<p>
-					Douglas Gerald Hurley is an American engineer, former Marine Corps
-					pilot and former NASA astronaut. He launched into space for the third
-					time as commander of Crew Dragon Demo-2.
-				</p>
+				<p>{crew[crewId].bio}</p>
 			</article>
 
-			<picture
-				srcSet="/src/assets/crew/image-douglas-hurley.webp"
-				type="image/webp"
-			>
-				<img
-					src="/src/assets/crew/image-douglas-hurley.png"
-					alt="Douglas Hurley"
-				/>
+			<picture srcSet={crew[crewId].images.webp} type="image/webp">
+				<img src={crew[crewId].images.png} alt={crew[crewId].name} />
 			</picture>
 		</main>
 	);
