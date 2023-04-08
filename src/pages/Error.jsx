f0a634bd63c7a8fcfd66f51b626d23f22d7de6db
0xEddie <eomeasoo@ualberta.ca>
2023-04-07 23:42:32 -0600

    error page styling

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -5,10 +5,10 @@ export default function Error() {
 	const error = useRouteError();
 
 	return (
-		<main id="main" className="error-page">
+		<main id="main" className="grid-container error-page">
 			{/* TODO add styling to error msg */}
-			<h1>lost?</h1>
-			<p>
+			<h1 className="ff-serif fs-800 uppercase text-accent">lost?</h1>
+			<p className="description fs-500">
 				Unexpected error:{' '}
 				<i>
 					{error.status} - {error.statusText}
