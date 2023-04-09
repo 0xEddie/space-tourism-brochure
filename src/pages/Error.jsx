4c103efa727a210053293a9cb083eed2df99cc33
0xEddie <eomeasoo@ualberta.ca>
2023-04-08 22:53:37 -0600

    error page styling

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -1,4 +1,4 @@
-import { useRouteError } from 'react-router-dom';
+import { Link, useRouteError } from 'react-router-dom';
 import './Error.css';
 
 export default function Error() {
@@ -7,13 +7,23 @@ export default function Error() {
 	return (
 		<main id="main" className="grid-container error-page">
 			{/* TODO add styling to error msg */}
-			<h1 className="ff-serif fs-800 uppercase text-accent">lost?</h1>
-			<p className="description fs-500">
-				Unexpected error:{' '}
-				<i>
+			<h1 className="ff-serif fs-800 uppercase text-accent letter-spacing-3">
+				lost?
+			</h1>
+
+			<article className="description text-white">
+				<h2 className="ff-sans fs-700">Unexpected error:</h2>
+				<i className="text-white fs-500">
 					{error.status} - {error.statusText}
 				</i>
-			</p>
+			</article>
+
+			<Link
+				to="/"
+				className="large-button uppercase ff-serif text-dark bg-white"
+			>
+				Home
+			</Link>
 		</main>
 	);
 }
